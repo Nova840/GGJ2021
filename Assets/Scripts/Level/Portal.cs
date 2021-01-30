c224029c452d4c4615f78e4f097a266c637aaa9f
Adam Zmudzinski <adamzmudzinski2@gmail.com>
2021-01-30 00:04:04 -0500

    Make portals that can take you to other portals.
    This works even from different scenes.

---
+++ Assets/Scripts/Level/Portal.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour {
+
+    [SerializeField]
+    private string portalName = "";
+
+    [SerializeField]
+    private string destinationSceneName = "";
+
+    [SerializeField]
+    private string destinationPortalName = "";
+
+    private static string lastDestinationPortalName = "start";//teleport to starting portal
+
+    private static Dictionary<string, Transform> spawnpointsForPortals = new Dictionary<string, Transform>();
+
+    private void Awake() {
+        if (spawnpointsForPortals.ContainsKey(portalName)) {
+            Debug.LogError("Two or more portals have the same name.");
+        }
+        spawnpointsForPortals.Add(portalName, transform.Find("Spawnpoint"));
+    }
+
+    private void OnDestroy() {
+        if (spawnpointsForPortals.ContainsKey(portalName)) {
+            spawnpointsForPortals.Remove(portalName);
+        }
+    }
+
+    private static Transform GetSpawnpointForPortal(string pName) {
+        if (!spawnpointsForPortals.ContainsKey(pName)) {
+            Debug.LogError("Portal \"" + pName + "\" does not exist in this scene.");
+            return null;
+        }
+        return spawnpointsForPortals[pName];
+    }
+
+    public void OnTriggerEnter(Collider other) {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player")) {
+            lastDestinationPortalName = destinationPortalName;
+            if (SceneManager.GetActiveScene().name != destinationSceneName) {
+                SceneManager.LoadScene(destinationSceneName);
+            } else {
+                TeleportToLastAssignedPortal(other.GetComponentInParent<CharacterController>());
+            }
+        }
+    }
+
+    public static void TeleportToLastAssignedPortal(CharacterController characterController) {
+        characterController.enabled = false;
+        characterController.transform.position = GetSpawnpointForPortal(lastDestinationPortalName).position;
+        characterController.transform.rotation = GetSpawnpointForPortal(lastDestinationPortalName).rotation;
+        characterController.enabled = true;
+    }
+
+}
--- Assets/Scripts/Player/PlayerSpawnOnStart.cs
+++ Assets/Scripts/Player/PlayerSpawnOnStart.cs
@@ -4,17 +4,8 @@ using UnityEngine;
 
 public class PlayerSpawnOnStart : MonoBehaviour {
 
-    private CharacterController characterController;
-
-    private void Awake() {
-        characterController = GetComponent<CharacterController>();
-    }
-
     private void Start() {
-        characterController.enabled = false;
-        transform.position = Spawnpoint.Point.position;
-        transform.rotation = Spawnpoint.Point.rotation;
-        characterController.enabled = true;
+        Portal.TeleportToLastAssignedPortal(GetComponent<CharacterController>());
     }
 
 }
